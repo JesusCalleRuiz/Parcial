c3fc4bc1bb30a8e0f7fc08624539eb61b0d2af3b
JesusCalleRuiz <146335396+JesusCalleRuiz@users.noreply.github.com>
2023-11-03 14:00:08 +0100

    Add files via upload

---
+++ types.ts
@@ -0,0 +1,11 @@
+export type Contacto = {
+    id: string;
+    dni: string;
+    name: string;
+    email: string;
+    postalCode: number;
+    country: string;
+    hour: string;
+    climate: string;
+    iso: string;
+  };
